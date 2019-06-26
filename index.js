2f2b1faec8440a0f7f643303afb477a316080509
Creep53 <creepminecraft53@gmail.com>
2019-06-26 11:31:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ bot.login("process.env.token");
 
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`);
-    client.user.setView("Cp help ");
+    client.user.setActivity("Cp help ");
 });
 
 client.on('message', async message => { 
